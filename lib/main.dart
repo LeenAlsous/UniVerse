b72184acffa79f1cab1d0ec5e6542f48892fcc21
LeenAlsous <leen.amer22@hotmail.com>
2023-12-23 18:09:36 +0300

    created repo and started with home page

---
+++ lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:sizer/sizer.dart';
+import 'package:uni_verse/l10n/app_locale.dart';
+import 'package:uni_verse/presentation/create_account.dart';
+import 'package:uni_verse/presentation/home_page.dart';
+import 'package:uni_verse/presentation/new_password.dart';
+import 'package:uni_verse/presentation/sign_in.dart';
+
+import 'generated/l10n.dart';
+
+void main() {
+  runApp(const UniVerse());
+}
+
+class UniVerse extends StatelessWidget {
+  const UniVerse({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ScopedModel<AppLocale>(
+        model: AppLocale(),
+        child: ScopedModelDescendant<AppLocale>(
+          builder: (widget, context, model) {
+            return Sizer(
+              builder: (context, orientation, deviceType){
+                return MaterialApp(
+                  debugShowCheckedModeBanner: false,
+                  locale: AppLocale().currentLocale,
+                  supportedLocales: S.delegate.supportedLocales,
+                  localizationsDelegates: [
+                    GlobalMaterialLocalizations.delegate,
+                    GlobalWidgetsLocalizations.delegate,
+                    GlobalCupertinoLocalizations.delegate,
+                    S.delegate
+                  ],
+                  theme: ThemeData(
+                    primaryColor: const Color(0xFF371942),
+                    textTheme: const TextTheme(
+                      bodyLarge: TextStyle(fontSize: 30),
+                      bodyMedium: TextStyle(fontSize: 14),
+                    ),
+                  ),
+                  home: const HomePage(),
+                  routes: {
+                    CreateAccount.route: (context) => const CreateAccount(),
+                    SignIn.route: (context) => const SignIn(),
+                    NewPassword.route: (context) => const NewPassword(),
+                    HomePage.route: (context) => const HomePage()
+                  },
+                );
+              },
+            );
+          },
+        ));
+  }
+}
---
+++ lib/presentation/home_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+import 'package:uni_verse/presentation/common/categories_container.dart';
+
+class HomePage extends StatelessWidget {
+  static String route = "HomePage";
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        width: 100.w,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Choose what you wanna manage', style: TextStyle(
+            fontSize:
+            Theme.of(context).textTheme.bodyLarge?.fontSize), textAlign: TextAlign.center,),
+            SizedBox(height: 3.h,),
+            CategoriesContainer(label: const Text('Time management'), icon: CupertinoIcons.time_solid, onTap: (){},),
+            SizedBox(
+              height: 5.h,
+            ),
+            CategoriesContainer(label: const Text('Budgeting'), icon: CupertinoIcons.time_solid, onTap: (){},),
+            SizedBox(
+              height: 5.h,
+            ),
+            CategoriesContainer(label: const Text('Map'), icon: CupertinoIcons.time_solid, onTap: (){},),
+            SizedBox(
+              height: 5.h,
+            ),
+            CategoriesContainer(label: const Text('Mental health'), icon: CupertinoIcons.time_solid, onTap: (){},),
+          ],
+        ),
+      ),
+    );
+  }
+}
