f158ad1dbc46dc91901fb0552ca4a9f451cc39dc
LeenAlsous <leen.amer22@hotmail.com>
2023-12-24 23:21:54 +0300

    time management ui only

--- lib/presentation/time_management_tabs/calendar.dart
+++ lib/presentation/time_management_tabs/calendar.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 class Calendar extends StatefulWidget {
-  const Calendar({Key? key}) : super(key: key);
+  const Calendar({super.key});
 
   @override
   State<Calendar> createState() => _CalendarState();
 }
 
 class _CalendarState extends State<Calendar> {
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
-    );
+        body: TableCalendar(
+      focusedDay: DateTime.now(),
+      firstDay: DateTime.utc(2000, 1, 1),
+      lastDay: DateTime.utc(2030, 3, 14),
+          selectedDayPredicate: (day) {
+            return isSameDay(_selectedDay, day);
+          },
+          onDaySelected: (selectedDay, focusedDay) {
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDay = focusedDay; // update `_focusedDay` here as well
+            });
+          },
+    ));
   }
 }
--- lib/presentation/time_management_tabs/to_do.dart
+++ lib/presentation/time_management_tabs/to_do.dart
@@ -1,10 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:uni_verse/presentation/common/to_do_tile.dart';
 
 class ToDo extends StatelessWidget {
   const ToDo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    TextEditingController todoController = TextEditingController();
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+         body: Column(
+           children: [
+             Align(
+               alignment: Alignment.topRight,
+               child: IconButton(
+                    onPressed: () {
+                      showModalBottomSheet(
+                          context: context,
+                          builder: (context) {
+                            return BottomSheet(
+                                onClosing: () {},
+                                builder: (context) {
+                                  return SizedBox(
+                                    height: 300,
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Text("Add to-do",
+                                            style: TextStyle(fontSize: 22)),
+                                        Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: TextField(
+                                            controller: todoController,
+                                            decoration: InputDecoration(
+                                                enabledBorder: OutlineInputBorder(
+                                                    borderSide: BorderSide(
+                                                        width: 3, color: Colors.blue),
+                                                    borderRadius:
+                                                        BorderRadius.circular(25)),
+                                                hintText: "To-Do"),
+                                          ),
+                                        ),
+                                        ElevatedButton(
+                                            onPressed: () {}, child: Text("Add"))
+                                      ],
+                                    ),
+                                  );
+                                });
+                          });
+                    },
+                    icon: Icon(Icons.add)),
+             ),
+             ToDoTile(label: "study")
+           ],
+         )
+    );
   }
 }
